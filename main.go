5b5985a49db228a1b62dbcbbca7989e97d21fe3b
Cameron Esfahani <1620786+d1r7y@users.noreply.github.com>
2022-12-22 21:52:54 -0800

    Initial checkin.

---
+++ main.go
@@ -0,0 +1,27 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"github.com/d1r7y/advent_2022/cmd"
+	_ "github.com/d1r7y/advent_2022/cmd/day01"
+	_ "github.com/d1r7y/advent_2022/cmd/day02"
+	_ "github.com/d1r7y/advent_2022/cmd/day03"
+	_ "github.com/d1r7y/advent_2022/cmd/day04"
+	_ "github.com/d1r7y/advent_2022/cmd/day05"
+	_ "github.com/d1r7y/advent_2022/cmd/day06"
+	_ "github.com/d1r7y/advent_2022/cmd/day07"
+	_ "github.com/d1r7y/advent_2022/cmd/day08"
+	_ "github.com/d1r7y/advent_2022/cmd/day09"
+	_ "github.com/d1r7y/advent_2022/cmd/day10"
+	_ "github.com/d1r7y/advent_2022/cmd/day11"
+	_ "github.com/d1r7y/advent_2022/cmd/day13"
+	_ "github.com/d1r7y/advent_2022/cmd/day16"
+	_ "github.com/d1r7y/advent_2022/cmd/day17"
+	_ "github.com/d1r7y/advent_2022/cmd/day21"
+)
+
+func main() {
+	cmd.Execute()
+}
